c17f88721af66a28c02ea8a13e3de3cd92f49003
NRRozborski <nrrozborski@gmail.com>
2026-05-16 18:17:46 +0200

    token

--- webApp/java/back/src/main/java/com/nz/portfolio/model/token.java
+++ webApp/java/back/src/main/java/com/nz/portfolio/model/token.java
@@ -1,8 +1,27 @@
 package com.nz.portfolio.model;
 
 public class token {
+    private int id;
+    private String token;
 
-    public token() {
+    public token(int id, String token) {
+        this.id = id;
+        this.token = token;
     }
-    
+
+    public int getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
