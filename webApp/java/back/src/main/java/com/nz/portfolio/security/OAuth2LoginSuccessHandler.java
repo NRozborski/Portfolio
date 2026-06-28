ed3b8bacca4c0179c2494fad971b98c8f2f08fce
NRRozborski <nrrozborski@gmail.com>
2026-06-29 01:33:51 +0200

    Oauth2 done 2

--- webApp/java/back/src/main/java/com/nz/portfolio/security/OAuth2LoginSuccessHandler.java
+++ webApp/java/back/src/main/java/com/nz/portfolio/security/OAuth2LoginSuccessHandler.java
@@ -1,27 +1,38 @@
 package com.nz.portfolio.security;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
+import com.nz.portfolio.service.UserService;
+
+import io.jsonwebtoken.io.IOException;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
- // private final UserService userService;
+  private final UserService userService;
 
- // public OAuth2LoginSuccessHandler(UserService userService) {
- //     super("/dashboard");
- //     this.userService = userService;
- // }
+  public OAuth2LoginSuccessHandler(UserService userService) {
+      super("/dashboard");
+      this.userService = userService;
+  }
 
- // @Override
- // public void onAuthenticationSuccess(HttpServletRequest request,
- //         HttpServletResponse response, Authentication authentication)
- //         throws IOException, ServletException {
- //     OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
- //     userService.saveOrUpdate(oidcUser.getEmail(), oidcUser.getAttribute("name"));
- //     super.onAuthenticationSuccess(request, response, authentication);
- // }
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request,
+          HttpServletResponse response, Authentication authentication)
+          throws IOException, ServletException {
+      OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
+      userService.saveOrUpdate(oidcUser.getEmail(), oidcUser.getAttribute("name"));
+      try{
+          super.onAuthenticationSuccess(request, response, authentication);
+      } catch (Exception e) {
+          System.out.println("oopsie");
+      }
+      //TODO handling of IOException
+  }
 }
--- webApp/java/back/src/main/java/com/nz/portfolio/security/SecurityConfig.java
+++ webApp/java/back/src/main/java/com/nz/portfolio/security/SecurityConfig.java
@@ -8,37 +8,42 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
- //   @Autowired
- //   private JwtUtil jwtUtil;   
- //   @Autowired
- //   private JwtAuthenticationFilter jwtFilter;
-//
- //   @Bean
- //   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
- //       http
- //           .authorizeHttpRequests(auth -> auth
- //               .requestMatchers("/", "/login").permitAll()
- //               .anyRequest().authenticated()
- //           )
- //           .formLogin(form -> form
- //               .loginPage("/login")
- //               .successHandler((req, res, auth) -> {
- //                   UserDetails userDetails = (UserDetails) auth.getPrincipal();
- //                   String token = jwtUtil.generateToken(userDetails);
- //                   res.getWriter().write("{\"token\":\"" + token + "\"}");
- //               })
- //               .permitAll()
- //           )
- //           .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/dashboard", true))
- //           .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
- //           .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
- //           .oauth2Login(oauth2 -> oauth2.successHandler(successHandler));
- //       return http.build();
- //   }
+    @Autowired
+    private JwtUtil jwtUtil;   
+    @Autowired
+    private JwtAuthenticationFilter jwtFilter;
+    @Autowired
+    AuthenticationSuccessHandler successHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        
+        
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .successHandler((req, res, auth) -> {
+                    UserDetails userDetails = (UserDetails) auth.getPrincipal();
+                    String token = jwtUtil.generateToken(userDetails);
+                    res.getWriter().write("{\"token\":\"" + token + "\"}");
+                })
+                .permitAll()
+            )
+            .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("/dashboard", true))
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+            .oauth2Login(oauth2 -> oauth2.successHandler(successHandler));
+        return http.build();
+    }
 
 }
